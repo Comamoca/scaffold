dca3d3d98189d4120ab8dc6ec5edd7226e5e8bb8
Comamoca <comamoca.dev@gmail.com>
2024-09-22 00:21:49 +0900

    :sparkles: (flake-basic) Update template to use flake-parts.

--- flake-basic/flake.nix
+++ flake-basic/flake.nix
@@ -1,63 +1,88 @@
 {
-  description = "A very basic flake";
+  description = "A basic flake to with flake-parts";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs =
-    {
+    inputs@{
       self,
+      systems,
       nixpkgs,
       treefmt-nix,
+      flake-parts,
     }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      stdenv = pkgs.stdenv;
-      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-    in
-    {
-      # Run treefmt. See `./treefmt.nix`.
-      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+    # let
+    #   pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    #   stdenv = pkgs.stdenv;
+    #   treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+    # in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ treefmt-nix.flakeModule ];
+      systems = import inputs.systems;
 
-      # When execute `nix develop`, you go in shell installed nil.
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [
-          nil
-        ];
-      };
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          stdenv = pkgs.stdenv;
 
-      templates.default = {
-        path = ./.;
-        description = "Nix flake basic template.";
-      };
+          # To make executable binary.
+          executable = stdenv.mkDerivation {
+            # Set executable binary name.
+            pname = "executable";
+            version = "0.0.1";
+            # Specify source path. You must specify the file added with `git add`.
+            src = ./.;
 
-      # To make executable binary.
-      executable = stdenv.mkDerivation {
-        # Set executable binary name.
-        pname = "executable";
-        version = "0.0.1";
-        # Specify source path. You must specify the file added with `git add`.
-        src = ./.;
-        # Write build commands. e.g. make, gcc, etc...
-        buildCommand = '''';
-      };
+            # Write build commands. e.g. make, gcc, etc...
+            buildPhase = '''';
 
-      # When execute `nix run`, print "Hello World!".
-      # And execute `nix build` to make execute at `./result/bin/hello`.
-      hello = stdenv.mkDerivation {
-        pname = "hello";
-        version = "0.1.0";
-        src = pkgs.writeShellScriptBin "hello" ''
-          echo Hello World!
-        '';
+            # Write build commands. e.g. install file $out/bin/file
+            installPhase = '''';
+          };
 
-        buildCommand = ''
-          install -D $src/bin/hello $out/bin/hello
-        '';
-      };
+          # When execute `nix run`, print "Hello World!".
+          # And execute `nix build` to make execute at `./result/bin/hello`.
+          hello = stdenv.mkDerivation {
+            pname = "hello";
+            version = "0.1.0";
+            src = pkgs.writeShellScriptBin "hello" ''
+              echo Hello World!
+            '';
 
-      packages.x86_64-linux.default = self.hello;
+            buildCommand = ''
+              install -D $src/bin/hello $out/bin/hello
+            '';
+          };
+        in
+        rec {
+          # When execute `nix fmt`, formatting your code.
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+            };
+
+            settings.formatter = { };
+          };
+
+          # When execute `nix develop`, you go in shell installed nil.
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nil
+            ];
+          };
+
+          packages.default = hello;
+        };
     };
 }
