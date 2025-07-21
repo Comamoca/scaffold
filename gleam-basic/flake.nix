9f207e6ded6b6176d9d74adf6928df30d692c09a
Comamoca <comamoca.dev@gmail.com>
2025-07-21 15:58:10 +0900

    :sparkles: (gleam-basic/) Add new template for Gleam language

---
+++ gleam-basic/flake.nix
@@ -0,0 +1,114 @@
+{
+  description = "A basic flake to with Gleam language";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    devenv.url = "github:cachix/devenv";
+    nix-gleam.url = "github:arnarg/nix-gleam";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      systems,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks-nix.flakeModule
+        inputs.devenv.flakeModule
+      ];
+      systems = import inputs.systems;
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          stdenv = pkgs.stdenv;
+	  
+          # hello = pkgs.buildGleamApplication {
+          #   src = ./.;
+          # };
+
+          git-secrets' = pkgs.writeShellApplication {
+            name = "git-secrets";
+            runtimeInputs = [ pkgs.git-secrets ];
+            text = ''
+              git secrets --scan
+            '';
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              nix-gleam.overlays.default
+            ];
+            config = { };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+            };
+
+            settings.formatter = { };
+          };
+
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              hooks = {
+                treefmt.enable = true;
+                ripsecrets.enable = true;
+                git-secrets = {
+                  enable = true;
+                  name = "git-secrets";
+                  entry = "${git-secrets'}/bin/git-secrets";
+                  language = "system";
+                  types = [ "text" ];
+                };
+              };
+            };
+          };
+
+          devenv.shells.default = {
+            packages = with pkgs; [
+	      nil
+	      beam28Packages.rebar
+	    ];
+
+            languages = {
+              gleam = {
+                enable = true;
+              };
+	      erlang = {
+                enable = true;
+	      };
+	      # javascript = {
+              #   enable = true;
+	      # };
+	      # deno = {
+              #   enable = true;
+	      # };
+            };
+
+            enterShell = '''';
+          };
+
+	  # packages.default = hello;
+        };
+    };
+}
